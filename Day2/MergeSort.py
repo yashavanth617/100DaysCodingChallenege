ef85832339299ddced815617d826f10d46090010
Yashavanth T V <150511905+yashavanth617@users.noreply.github.com>
2025-08-06 17:06:29 +0530

    Create MergeSort.py

---
+++ Day2/MergeSort.py
@@ -0,0 +1,40 @@
+# Merge Sort
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i<len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] =left_half[i]
+                i+=1
+            else:
+                arr[k] = right_half[j]
+                j+=1
+            k+=1
+
+            # Remaining elements of Left half
+        while i < len(left_half):
+                arr[k] = left_half[i]
+                i += 1
+                k += 1
+
+            # Remaining elements of Right half
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+    else:
+         return arr
+
+x = [78,46,40,1,2,4,5]
+
+merge_sort(x)
+print(x)
