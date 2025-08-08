4a2f9a998fa4d91d2c6316ec56df6478ced46e78
Yashavanth T V <150511905+yashavanth617@users.noreply.github.com>
2025-08-08 16:12:25 +0530

    Create  DeleteElementByIndexInList.py

---
+++ Day5/DeleteElementByIndexInList.py
@@ -0,0 +1,18 @@
+def DeleteElementByIndexInList(x, index):
+    if index > len(x):
+        print("out of index range")
+        return
+    
+    c= 0
+    l = [0] * (len(x)-1)
+    for i in range(index-1):
+        l[c] = x[i]
+        c+=1
+
+    for i in range(index, len(x)):
+        l[c] = x[i]
+        c+=1
+
+    print(l)
+x = [1,2,3,4,5,6]
+d = DeleteElementByIndexInList(x,4)
