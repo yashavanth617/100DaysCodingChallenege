91dfb865a32f7362c6c8082a71cf0390a6ca6b2b
Yashavanth T V <150511905+yashavanth617@users.noreply.github.com>
2025-08-10 16:43:18 +0530

    Create DoublyLinkedListForward.py

---
+++ Day7/DoublyLinkedListForward.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.root = None
+        self.top = None
+
+    def InsertNode(self, val):
+        n = Node(val)
+        if self.root is None:
+            self.root = n
+            self.top= n
+            return
+        
+        self.top.next = n
+        n.prev = self.top
+        self.top = n
+        
+    def DisplayForward(self):
+        temp = self.root
+        while temp is not None:
+            print(temp.val, end = "<->")
+            temp = temp.next
+        print("None")
+      
+dll = DoublyLinkedList()
+dll.InsertNode(10)
+dll.InsertNode(20)
+dll.InsertNode(30)
+dll.InsertNode(40)
+dll.DisplayForward()
