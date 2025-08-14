6539e4092755c38e881760f888afc25f6f292bd8
Yashavanth T V <150511905+yashavanth617@users.noreply.github.com>
2025-08-14 14:36:50 +0530

    Update MergTwoSortedToSingleSortArray.py

--- Day11/MergTwoSortedToSingleSortArray.py
+++ Day11/MergTwoSortedToSingleSortArray.py
@@ -1,26 +1,27 @@
-def MergeTwoSortedArray(arr1, arr2):
-    i = j = 0
-    merge = []
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1_copy = nums1[:m]
+        i = j = 0
+        k=0
+        while i < m and j < n:
+           
+            if nums1_copy[i] < nums2[j]:
+                nums1[k] =nums1_copy[i]
+                i += 1
 
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] < arr2[j]:
-            merge.append(arr1[i])
+            else:
+                nums1[k]=nums2[j]
+                j += 1
+            k += 1
+        while i < m:
+            nums1[k] =nums1_copy[i]
             i += 1
-        else:
-            merge.append(arr2[j])
-            j += 1
+            k +=1
         
-    while i < len(arr1):
-        merge.append(arr1[i])
-        i += 1
-
-    while j < len(arr2):
-        merge.append(arr2[j])
-        j += 1
-    
-    print(merge)
-
-arr1 = [1,2,8,9,9,22,35,37,38]
-arr2 = [2,3,4,5,6,7,20,21,33,36,39,40,45,88]
-
-MergeTwoSortedArray(arr1, arr2)
+        while j < n:
+            nums1[k]=nums2[j]
+            j += 1
+            k +=1
